5a406cacbf72f4cf44b91b56efd4640c06f00520
ekoda-afk <tomomiekoda561@gmail.com>
2025-07-27 10:33:06 +0630

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ function showSection(section) {
 window.onload = function() {
   showSection('home');
 };
+
+window.onload = function() {
+  
+  const tg = window.Telegram.WebApp;
+
+  showSection('home');
+
+  if (tg && tg.initDataUnsafe && tg.initDataUnsafe.user) {
+    const user = tg.initDataUnsafe.user;
+    document.getElementById('username').innerText = user.first_name + (user.last_name || '');
+    document.getElementById('user-id').innerText = user.id;
+  } else {
+    document.getElementById('username').innerText = "Unknown";
+    document.getElementById('user-id').innerText = "N/A";
+  }
+};
